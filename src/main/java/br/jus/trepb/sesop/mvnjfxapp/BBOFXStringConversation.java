3211a0c8e3e59ac7c867f880657c6e6c3d4ffe68
rogerlais <rogerlais@gmail.com>
2017-09-10 12:38:37 -0300

    initial formateDate(tentaive)

--- src/main/java/br/jus/trepb/sesop/mvnjfxapp/BBOFXStringConversation.java
+++ src/main/java/br/jus/trepb/sesop/mvnjfxapp/BBOFXStringConversation.java
@@ -17,22 +17,24 @@ import java.util.TimeZone;
  */
 public class BBOFXStringConversation extends DefaultStringConversion {
 
+    private TimeZone LocalTimeZone = TimeZone.getTimeZone("BRT");
+
     @Override
     protected String formatTime(Time time) {
         // todo formtar de acordo com a regra do BB
         GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone("BRT"));
         calendar.setTime(time);
-        String result = String.format("%1$tH%1$tM%1$tS[%Z:%z]", calendar);
+        String result = String.format("%1$tH%1$tM%1$tS[%Z:%z]", calendar) + "[-3BRT]";
         return result;
         // !!!OLD FORM - SEE ABOVE return String.format("%1$tH%1$tM%1$tS.%1$tL", calendar);
     }
 
     @Override
     protected String formatDate(Date date) {
-        // todo formatar de acordo com a regr ado BB
-        GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone("BRT"));
+        // todo formatar de acordo com a regras do BB
+        GregorianCalendar calendar = new GregorianCalendar(this.LocalTimeZone);
         calendar.setTime(date);
-        String result = String.format("%1$tY%1$tm%1$td%1$tH%1$tM%1$tS[%Z:%z]", calendar);
+        String result = String.format("%1$tY%1$tm%1$td%1$tH%1$tM%1$tS", calendar) + "[-3BRT]";
         return result;
     }
 
