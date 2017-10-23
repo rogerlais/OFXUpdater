779a208096c6ee658aec386460c45895ea403a3f
rogerlais <rogerlais@gmail.com>
2017-10-22 23:52:35 -0300

    cc transactions writer

--- src/main/java/br/jus/trepb/sesop/mvnjfxapp/OFXCreditCardBilling.java
+++ src/main/java/br/jus/trepb/sesop/mvnjfxapp/OFXCreditCardBilling.java
@@ -8,13 +8,14 @@ package br.jus.trepb.sesop.mvnjfxapp;
 import com.webcohesion.ofx4j.domain.data.MessageSetType;
 import com.webcohesion.ofx4j.domain.data.ResponseEnvelope;
 import com.webcohesion.ofx4j.domain.data.ResponseMessage;
-import com.webcohesion.ofx4j.domain.data.creditcard.CreditCardResponseMessageSet;
-import com.webcohesion.ofx4j.domain.data.creditcard.CreditCardStatementResponse;
+import com.webcohesion.ofx4j.domain.data.common.Transaction;
 import com.webcohesion.ofx4j.domain.data.creditcard.CreditCardStatementResponseTransaction;
 import com.webcohesion.ofx4j.io.AggregateUnmarshaller;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.Reader;
 import java.util.Iterator;
 import java.util.List;
@@ -38,12 +39,13 @@ public class OFXCreditCardBilling {
         return ret;
     }
 
-    public void exportTo(String destFilename) throws OFXException {
+    public void exportTo(String destFilename) throws OFXException, FileNotFoundException {
+        StringBuilder sb = new StringBuilder();
         try {
             try (
-                FileInputStream fis = new FileInputStream(this.sourceFile);
-                Reader fr = new InputStreamReader(fis, "windows-1252") //Requerido para interpertrar corretamente os dados
-                ) //allocation
+                    FileInputStream fis = new FileInputStream(this.sourceFile);
+                    Reader fr = new InputStreamReader(fis, "windows-1252") //Requerido para interpertrar corretamente os dados
+                    ) //allocation
             {
                 AggregateUnmarshaller a = new AggregateUnmarshaller(ResponseEnvelope.class);  //Adapter para serialização
                 this.OFXContent = (ResponseEnvelope) a.unmarshal(fr);
@@ -53,17 +55,41 @@ public class OFXCreditCardBilling {
         }
         //(CreditCardStatementResponseTransaction)
         List<ResponseMessage> accountList = this.OFXContent.getMessageSet(MessageSetType.creditcard).getResponseMessages();
-        for (Iterator<ResponseMessage> accounts = accountList.iterator(); accounts.hasNext();) {  //Varre as contas presentes, para o amex serão 2
+        for (Iterator<ResponseMessage> it = accountList.iterator(); it.hasNext();) {
+            CreditCardStatementResponseTransaction responseMessage = (CreditCardStatementResponseTransaction) it.next();
+            List<Transaction> transList = responseMessage.getMessage().getTransactionList().getTransactions();
+            for (Transaction transaction : transList) {
+                sb.append(transaction.getDatePosted());
+                sb.append("\t");
+                sb.append(transaction.getAmount()); //inverter sinal para obter significado
+                sb.append("\t");
+                sb.append(transaction.getMemo());
+                sb.append("\r\n");
+                'saida a corrigir abaixo'
+                /*
 
-            ResponseMessage responseMessage = (ResponseMessage) accounts.next();
-            CreditCardStatementResponse itemX = responseMessage.getMessage();
+                Mon Aug 21 00:00:01 GMT-03:00 2017	3013.54	SALDO ANTERIOR
+                Sat Nov 25 00:00:01 GMT-03:00 2017	160.55	CARREFOUR.COM 10/1010/10
+                Fri Jan 13 00:00:01 GMT-03:00 2017	18.14	EXTRA.COM 8/88/8
+                Sat Mar 18 00:00:01 GMT-03:00 2017	318.98	WALMART COM ELETRONICO 6/96/9
+                Thu May 04 00:00:01 GMT-03:00 2017	129.89
+
+                 */
+            }
+        }
+        if (sb.length() > 1) {
+            try (PrintWriter out = new PrintWriter(destFilename)) {
+                out.println(sb.toString());
+            }
         }
 
+        /*
         CreditCardResponseMessageSet items = (CreditCardResponseMessageSet) this.OFXContent.getMessageSet(MessageSetType.creditcard);
         List<ResponseMessage> transList = this.OFXContent.getMessageSet(MessageSetType.creditcard).getResponseMessages();
         for (ResponseMessage item : transList) {
             item.getResponseMessageName();
         }
+         */
     }
 
 }
